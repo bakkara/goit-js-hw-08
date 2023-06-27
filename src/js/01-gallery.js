3dcf383724b4f0dbe693452a30ba38fcb997a74b
bakkara <ane4karymar@gmail.com>
2023-06-27 09:05:35 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createMarkupItem(arr) {
+   return arr.map(({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}" rel="noreferrer noopener">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join(''); 
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', createMarkupItem(galleryItems))
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
