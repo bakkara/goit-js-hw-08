f830713b7602ec3d6d78b79281369912c9016fb1
bakkara <ane4karymar@gmail.com>
2023-06-29 20:01:43 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,10 +14,12 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
   localStorage.setItem(KEY_CURRENT_TIME, seconds);
 }
-const savedTime = localStorage.getItem(KEY_CURRENT_TIME);
-if (savedTime !== null) {
-  player.setCurrentTime(savedTime);
-} else {
-  player.setCurrentTime(0);
-}
+// const savedTime = localStorage.getItem(KEY_CURRENT_TIME);
+// if (savedTime !== null) {
+//   player.setCurrentTime(savedTime);
+// } else {
+//   player.setCurrentTime(0);
+// }
 // player.setCurrentTime(localStorage.getItem(KEY_CURRENT_TIME)) ?? player.setCurrentTime(0)
+
+player.setCurrentTime(localStorage.getItem(KEY_CURRENT_TIME) || 0)
