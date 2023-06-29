f1b483aa9576ab7feb01b056bc0c0cfe7d97ef01
bakkara <ane4karymar@gmail.com>
2023-06-29 17:43:29 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const KEY_FEEDBACK = "feedback-form-state";
+
+let data = {};
+
+form.addEventListener('input', throttle(onInput, 500))
+form.addEventListener('submit', submitForm)
+window.addEventListener('load', loadPage)
+
+function onInput(evt) {
+    data[evt.target.name] = evt.target.value
+    localStorage.setItem(KEY_FEEDBACK, JSON.stringify(data))
+}
+
+function submitForm(evt) {
+    evt.preventDefault();
+    if (data.email && data.message) {
+        console.log(data);
+        evt.currentTarget.reset()
+        localStorage.removeItem(KEY_FEEDBACK);
+        data = {}
+    } else {
+        alert('Заповнені не всі поля!');
+    }
+}
+
+function loadPage() {
+    const saveData = JSON.parse(localStorage.getItem(KEY_FEEDBACK))
+    if (!saveData) {
+        return;
+    }
+    form.elements.email.value = saveData.email ?? "";
+    form.elements.message.value = saveData.message ?? "";
+}
