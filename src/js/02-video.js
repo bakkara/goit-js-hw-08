00d4eae4b0f2c1388879724e029cdb10d05ad9a1
bakkara <ane4karymar@gmail.com>
2023-06-29 19:35:03 +0300

    fix task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+
 const throttle = require('lodash.throttle');
 
 const KEY_CURRENT_TIME = "videoplayer-current-time";
@@ -14,5 +14,10 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
   localStorage.setItem(KEY_CURRENT_TIME, seconds);
 }
-
-player.setCurrentTime(localStorage.getItem(KEY_CURRENT_TIME)) ?? player.setCurrentTime(0)
+const savedTime = localStorage.getItem(KEY_CURRENT_TIME);
+if (savedTime !== null) {
+  player.setCurrentTime(savedTime);
+} else {
+  player.setCurrentTime(0);
+}
+// player.setCurrentTime(localStorage.getItem(KEY_CURRENT_TIME)) ?? player.setCurrentTime(0)
